726e79c3ff88e8cff1c8833e22105083335dcded
Sia <siakaramalegos@gmail.com>
2019-04-12 17:33:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import sampleSize from 'lodash/sampleSize'
+const words = require('./words.json')
+
+function getPhrases(count) {
+  return sampleSize(words, count).join(' ')
+}
+
+function getRandomInt(min=3, max=5) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function makeSentence(words) {
+  return words.charAt(0).toUpperCase() + words.slice(1) + '.'
+}
+
+function getSentences(count) {
+  const sentenceArray = new Array(count).fill().map( element => {
+    const phraseCount = getRandomInt(6, 10)
+    const phrases = getPhrases(phraseCount)
+
+    return makeSentence(phrases)
+  })
+  return sentenceArray.join(' ')
+}
+
+function getParagraphElements(count) {
+  return new Array(count).fill().map( (e, i) => {
+    const sentenceCount = getRandomInt(3, 5)
+    const sentences  = getSentences(sentenceCount)
+    const paragraphElement = document.createElement('p')
+
+    paragraphElement.innerHTML = sentences
+    return paragraphElement
+  })
+}
+
+document.getElementById('phrase-form').onsubmit = function(e) {
+  e.preventDefault()
+  const count = parseInt(document.getElementById('phrase-count').value, 10)
+  const outputContainer = document.querySelector('#phrase-output')
+
+  outputContainer.innerHTML = getPhrases(count)
+}
+
+document.getElementById('paragraph-form').onsubmit = function (e) {
+  e.preventDefault()
+  const count = parseInt(document.getElementById('paragraph-count').value, 10)
+  const paragraphNodes = getParagraphElements(count)
+  const outputContainer = document.querySelector('#paragraph-output')
+
+  paragraphNodes.forEach(element => {
+    outputContainer.appendChild(element)
+  });
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        // use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devtool: 'source-map',
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true,
+    open: true
+  }
+};
