d78532c36a7be050512a6ef5cc562108e171ad3c
Sia <siakaramalegos@gmail.com>
2019-05-28 19:04:17 -0500

    Add tests

---
+++ test/test.js
@@ -0,0 +1,66 @@
+const expect = require('chai').expect
+const phrase = require('../index').phrase
+const sentence = require('../index').sentence
+const paragraph = require('../index').paragraph
+
+describe('coder-ipsum', function () {
+  describe('#phrase() with no input', function () {
+    it('should return a string', function () {
+      expect(phrase()).to.be.a('string');
+    });
+    it('should have a length greater than 3', function () {
+      expect(phrase()).to.have.lengthOf.above(3);
+    });
+  });
+  describe('#phrase() with integer input', function () {
+    it('should return a string', function () {
+      expect(phrase(3)).to.be.a('string');
+    });
+    it('should have a length greater than 3', function () {
+      expect(phrase(3)).to.have.lengthOf.above(3);
+    });
+  });
+  describe('#sentence() with no input', function () {
+    it('should return a string', function () {
+      expect(sentence()).to.be.a('string');
+    });
+    it('should have a length greater than 4', function () {
+      expect(sentence()).to.have.lengthOf.above(4);
+    });
+  });
+  describe('#sentence() with integer input', function () {
+    it('should return a string', function () {
+      expect(sentence(3)).to.be.a('string');
+    });
+    it('should have a length greater than 4', function () {
+      expect(sentence(3)).to.have.lengthOf.above(4);
+    });
+    it('should have a period at the end', function () {
+      const testSentence = sentence(3)
+      const lastCharacter = testSentence[testSentence.length - 1]
+      const numPeriods = (lastCharacter.match(/\./g) || []).length
+      expect(numPeriods).to.equal(1);
+    });
+  });
+  describe('#paragraph() with no input', function () {
+    it('should return a string', function () {
+      expect(paragraph()).to.be.a('string');
+    });
+    it('should have a length greater than 12', function () {
+      expect(paragraph()).to.have.lengthOf.above(12);
+    });
+  });
+  describe('#paragraph() with integer input', function () {
+    it('should return a string', function () {
+      expect(paragraph(3)).to.be.a('string');
+    });
+    it('should have a length greater than 12', function () {
+      expect(paragraph(3)).to.have.lengthOf.above(12);
+    });
+    it('should have three periods for three sentences', function () {
+      // If any of the input words has a period, this test will fail sometimes.
+      const numPeriods = (paragraph(3).match(/\./g) || []).length
+      expect(numPeriods).to.equal(3);
+    });
+  });
+});
