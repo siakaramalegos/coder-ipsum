e687493ba354c1f74e42aea7a88cd2e5938cdfed
Sia <siakaramalegos@gmail.com>
2019-05-31 16:40:42 -0500

    Adds mad libs and new domain

--- readme.md
+++ readme.md
@@ -4,16 +4,8 @@ So what had happened was...
 
 I was building a dumb web app to teach people how to make it faster, but none of the existing ipsums really felt right for a workshop for developers. I got together with some friends and brainstormed computer science thought-leadership synergistic words, and this is what came out.
 
-The website version is deployed [here](https://siakaramalegos.github.io/coder-ipsum/).
+The website version is deployed [here](https://coder-ipsum.tech).
 
-## Contributing
-
-Yes, you can contribute! Features I'd like to develop:
-
-- Turn this into an npm package so it's a tool like the [Faker Ruby gem](https://github.com/stympy/faker).
-- Provide a Mad-Lib option with fake sentences (subject, verb, object, etc.).
-
-If you have other ideas, feel free to contact me on [Twitter](https://twitter.com/TheGreenGreek).
 
 ## Thanks
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 require('./style.css')
-import {phrase, paragraph} from 'coder-ipsum'
+import {phrase, paragraph, madLib} from 'coder-ipsum'
 
 function getParagraphElements(count) {
   return new Array(count).fill().map( (e, i) => {
     const paragraphElement = document.createElement('p')
     paragraphElement.innerHTML = paragraph()
+    return paragraphElement
+  })
+}
+
+function getMadLibParagraphElements(count) {
+  return new Array(count).fill().map((e, i) => {
+    const paragraphElement = document.createElement('p')
+    paragraphElement.innerHTML = madLib()
     return paragraphElement
   })
 }
@@ -22,6 +30,23 @@ document.getElementById('paragraph-form').onsubmit = function (e) {
   const count = parseInt(document.getElementById('paragraph-count').value, 10)
   const paragraphNodes = getParagraphElements(count)
   const outputContainer = document.querySelector('#paragraph-output')
+
+  // Clear output and then replace with new paragraphs
+  outputContainer.innerHTML = ""
+
+  paragraphNodes.forEach(element => {
+    outputContainer.appendChild(element)
+  });
+}
+
+document.getElementById('madlibs-form').onsubmit = function (e) {
+  e.preventDefault()
+  const count = parseInt(document.getElementById('madlibs-count').value, 10)
+  const paragraphNodes = getMadLibParagraphElements(count)
+  const outputContainer = document.querySelector('#madlibs-output')
+
+  // Clear output and then replace with new paragraphs
+  outputContainer.innerHTML = ""
 
   paragraphNodes.forEach(element => {
     outputContainer.appendChild(element)
